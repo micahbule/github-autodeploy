bc172036564823a4cf59791b24197cd612feff9f
Marco Padillo <marco@cynder.io>
2015-11-23 13:26:15 +0800

    Fixed indentation

--- projects.js
+++ projects.js
@@ -1,11 +1,11 @@
 module.exports = [
-	{
-		name: 'project_name',
-		repository: 'User/repository-name',
-		branch_to_watch: 'master',
+    {
+        name: 'project_name',
+        repository: 'User/repository-name',
+        branch_to_watch: 'master',
         commands_to_run: [
             'npm install',
             'bower install --allow-root'
         ]
-	}
+    }
 ];
